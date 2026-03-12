b97d2ffd2e354dc4fa6a887a7d23b882ec4b3568
AstreinidisZafeirios <eastreinidis@gmail.com>
2026-03-12 05:38:41 +0200

    average depth task class constants and function definitions

--- include/tasks/average_depth_accuracy.h
+++ include/tasks/average_depth_accuracy.h
@@ -8,8 +8,14 @@ https://github.com/AlgTUDelft/pystreed
 
 namespace SORTD {
 
+	struct AVDAccExtraData {
+	        int unique_feature_vector_id{ 0 };
+	        static AVDAccExtraData ReadData(std::istringstream& iss, int num_labels) {return {};}	
+	};
+
         class AverageDepthAccuracy : public Classification {
         public:
+		using ET = AVDAccExtraData;
                 using SolType = double;                    // The data type of the solution
                 using SolD2Type = double;                  // The data type of the solution in the terminal solver
 		using BranchSolD2Type = double; 
@@ -19,27 +25,65 @@ namespace SORTD {
                 static constexpr int worst = INT32_MAX;         // An UB for the worst solution value possible
                 static constexpr double best = 0;                          // A LB for the best solution value possible
                 static constexpr int minimum_difference = 1;// The minimum difference between two solutions
+		static constexpr bool leaf_penalty = true;
+		static const bool has_branching_costs = false; 
+		static const bool element_branching_costs = false;
+	        static const bool preprocess_data = true;
+	        static const bool preprocess_train_data = true; 
+	        static const bool use_terminal = false;
+	        static const bool terminal_filter = false;
+	        static const bool custom_lower_bound = true;	
 
                 AverageDepthAccuracy(const ParameterHandler& parameters) : Classification(parameters) {}
+
+		inline void UpdateParameters(const ParameterHandler& parameters) {
+			cost_complexity_parameter = std::max(0.0, parameters.GetFloatParameter("cost-complexity"));
+	                lower_bound_cache.resize(parameters.GetIntegerParameter("max-depth") + 1);		
+		}
 
                 // Compute the leaf costs for the data in the context when assigning label
                 double GetLeafCosts(const ADataView& data, const BranchContext& context, int label) const;
 
                 // Compute the test leaf costs for the data in the context when assigning label
-                inline int GetTestLeafCosts(const ADataView& data, const BranchContext& context, int label) const {
-                        return GetLeafCosts(data, context, label);
-                }
+                inline int GetTestLeafCosts(const ADataView& data, const BranchContext& context, int label) const; 
                 // Compute the leaf costs for an instance given a assigned label
                 inline void GetInstanceLeafD2Costs(const AInstance* instance, int org_label, int label, double& costs, int multiplier) const { costs = multiplier * ((org_label == label) ? 0 : 1); }
 
+		double GetBranchingCosts(const BranchContext& context, int feature) const {
+			if constexpr (leaf_penalty) {
+				int multiplier = 1;
+				if (context.GetBranch().Depth() == 0) multiplier = 2;
+				return cost_complexity_parameter * train_summary.size * multiplier;
+			} else  {
+				return cost_complexity_parameter * train_summary.size;
+			}
+		}			
+
                 // Compute the solution value from a terminal solution value
-                void ComputeD2Costs(const int& d2costs, int count, double& costs, std::optional<BranchContext> context = std::nullopt) const { costs = d2costs; }
+                void ComputeD2Costs(const int& d2costs, int count, double& costs, std::optional<BranchContext> context = std::nullopt) const { 
+			if constexpr (leaf_penalty) {
+				if (context && context->GetBranch().Depth() == 0) {
+					costs = d2costs + cost_complexity_parameter * count;
+				} else {
+					costs = d2costs;
+				}
+			} else {
+				costs = d2costs;
+			}
+		}
 
                 // Return true if the terminal solution value is zero
-                inline bool IsD2ZeroCost(const int d2costs) const { return d2costs == 0; }
+                inline bool IsD2ZeroCost(const double d2costs) const { return std::abs(d2costs) <= 1e-6; }
+
+		double ComputeD2BranchingCosts(const double& d2costs, int count) const { return d2costs; }
 
                 // Get a bound on the worst contribution to the objective of a single instance with label
                 inline int GetWorstPerLabel(int label) const { return 1; }
+
+		Node<CostComplexAccuracy> CostComplexAccuracy::ComputeLowerBound(const ADataView& data, const Branch& branch, int max_depth, int num_nodes) ;
+
+		void PreprocessData(AData& data, bool train);
+		void PreprocessTrainData(ADataView& train_data);
 
                 // Compute the train score from the training solution value
                 inline double ComputeTrainScore(int test_value) const { return ((double)(train_summary.size - test_value)) / ((double)train_summary.size); }
@@ -49,6 +93,8 @@ namespace SORTD {
 
                 // Compute the test score on the test data from the test solution value
                 inline double ComputeTestTestScore(int test_value) const { return ((double)(test_summary.size - test_value)) / ((double)test_summary.size); }
+
+		static TuneRunConfiguration GetTuneRunConfiguration(const ParameterHandler& default_config, const ADataView& train_data, int phase);
         };
 
 }
--- src/tasks/average_depth_accuracy.cpp
+++ src/tasks/average_depth_accuracy.cpp
@@ -13,7 +13,114 @@ namespace SORTD {
                         if (k == label) continue;
                         error += data.NumInstancesForLabel(k);
                 }
+		int depth = context.GetBranch().Depth();
+		int portion data.Size();
+		double penalty = ((double) (depth * portion)) / ((double) (train_summary.size));
+		error += cost_complexity_parameter * penalty;
                 return error;
         }
+
+	int AverageDepthAccuracy::GetTestLeafCosts(const ADataView& data, const BranchContext& context, int label) const {
+		int error = 0;
+	        for (int k = 0; k < data.NumLabels(); k++) {
+			if (k == label) continue;
+	                error += data.NumInstancesForLabel(k);		
+		}	
+		return error;
+	}
+
+	Node<CostComplexAccuracy> CostComplexAccuracy::ComputeLowerBound(const ADataView& data, const Branch& branch, int max_depth, int num_nodes) {
+				// Equivalent Points Bound adapted from Angelino, E., Larus-Stone, N., Alabi, D., Seltzer, M., & Rudin, C. (2018). 
+				// 		// Learning certifiably optimal rule lists for categorical data. Journal of Machine Learning Research, 18(234), 1-78.
+				auto lb = Node<CostComplexAccuracy>(best);
+				auto& hashmap = lower_bound_cache[branch.Depth()];
+				auto it = hashmap.find(branch);
+				if (it != hashmap.end()) {
+				             return hashmap[branch];
+				}
+				lb.solution = 0;
+				lb.num_nodes_left = 0;
+				lb.num_nodes_right = 0;
+				const int num_labels = data.NumLabels();
+				std::vector<std::vector<const AInstance*>::const_iterator> iterators;
+				std::vector<std::vector<const AInstance*>::const_iterator> ends;
+				for (int k = 0; k < num_labels; k++) {
+					iterators.push_back(data.GetInstancesForLabel(k).begin());
+				        ends.push_back(data.GetInstancesForLabel(k).end());
+				}
+				std::vector<int> labels(num_labels);
+				std::iota(labels.begin(), labels.end(), 0);
+				auto comp_with_check_end = [&iterators, &ends](const int k1, const int k2) {
+					if (iterators[k1] == ends[k1]) return false;
+					if (iterators[k2] == ends[k2]) return true;
+					return (*iterators[k1])->GetID() < (*iterators[k2])->GetID();
+				};
+				auto comp = [&iterators, &ends](const int k1, const int k2) {
+					return (*iterators[k1])->GetID() < (*iterators[k2])->GetID();
+				};
+				std::sort(labels.begin(), labels.end(), comp_with_check_end);
+				// pop empty labels 
+				while (iterators[labels[labels.size() - 1]] == ends[labels[labels.size() - 1]])
+					labels.pop_back();
+				// Initialize prev with the first instance in order of the features
+				std::vector<int> class_counts(num_labels, 0);
+				int current_label = labels[0];
+				const AInstance* prev = *iterators[current_label];
+				class_counts[current_label]++;
+				iterators[current_label]++;
+				int n = 1;
+				// while the first iterator still has instances
+				while (labels.size() > 0 && iterators[labels[0]] != ends[labels[0]]) {
+				        current_label = labels[0];
+					if (static_cast<const Instance<int, CCAccExtraData>*>(prev)->GetExtraData().unique_feature_vector_id ==
+				            static_cast<const Instance<int, CCAccExtraData>*>(*iterators[current_label])->GetExtraData().unique_feature_vector_id) {
+						class_counts[current_label]++;
+				         	n++;
+					} else {
+						if (n > 1) {
+				                     //check if the label count is unique
+				          		int largest = class_counts[0];
+							for (int k = 1; k < num_labels; k++) {
+								if (class_counts[k] > largest) {
+									largest = class_counts[k];
+								}
+							}
+							int min_error = n - largest;
+							lb.solution += min_error;
+						}
+				                std::fill(class_counts.begin(), class_counts.end(), 0);
+						class_counts[current_label]++;
+						n = 1;
+					}
+				        prev = *iterators[current_label];
+					iterators[current_label]++;
+					if (iterators[current_label] == ends[current_label]) {
+						labels.erase(labels.begin());
+						continue;
+					}
+				        //std::sort(labels.begin(), labels.end(), comp);
+					for (int k = 1; k < labels.size(); k++) {
+						if (comp(labels[k], current_label)) {
+				          		std::swap(labels[k], labels[k-1]);
+						} else {
+				          		break;
+				        	}
+				        }			
+				}
+				// Compute the error from the last group
+				if (n > 1) {
+				      // Check if the label count is unique
+				      int largest = class_counts[0];
+				      for (int k = 1; k < num_labels; k++) {
+						if (class_counts[k] > largest) {
+							largest = class_counts[k];
+						}
+				      }
+				      int min_error = n - largest;
+				      lb.solution += min_error;
+				}
+				hashmap[branch] = lb;
+				return lb;
+		}
 
 }
